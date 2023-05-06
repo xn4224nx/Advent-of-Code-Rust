4ec4a7c164206a7f266cac00fc839755ef72b849
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-05-06 01:35:23 +0100

    Day 03 - Part 2 - Completed

--- 2018/day_03/src/main.rs
+++ 2018/day_03/src/main.rs
@@ -5,6 +5,9 @@
 *	Part 1 - How many square inches of fabric are within two or 	*
 * 		more claims?						*
 *									*
+*	Part 2 - What is the ID of the only claim that doesn't 		*
+* 		overlap?						*
+* 									*			
 ************************************************************************/
 
 
@@ -13,15 +16,16 @@ use std::io::{self, BufRead};
 use std::path::Path;
 use regex::Regex;
 use std::collections::HashMap;
+use std::collections::HashSet;
 
 
 /// Each claim has an id and a rectangular shape in a 2D grid.
 #[derive(Debug)]
 struct Claim {
     id: i32,
-    x_pos: i32,
-    y_pos: i32,
-    wide: i32,
+    x: i32,
+    y: i32,
+    width: i32,
     height: i32,
 }
 
@@ -35,8 +39,8 @@ impl Claim {
 	// Storage for all the points in the claim
 	let mut points = Vec::new();
 	
-	for i in self.x_pos..(self.x_pos + self.wide) {
-	    for j in self.y_pos..(self.y_pos + self.height) {
+	for i in self.x..(self.x + self.width) {
+	    for j in self.y..(self.y + self.height) {
 		points.push((i, j));
 	    }
 	}
@@ -71,8 +75,6 @@ fn main() {
 	    
 	    if let Ok(ip_line) = line {
 		
-		//println!("{}", ip_line);
-		
 		/* Parse the line into the Claim structure */
 		let cap = re.captures(&ip_line).unwrap();
 		
@@ -83,17 +85,17 @@ fn main() {
 					    .parse::<i32>()
 					    .unwrap(),
 		    
-		    x_pos: 	cap.get(2).unwrap()
+		    x: 		cap.get(2).unwrap()
 					    .as_str()
 					    .parse::<i32>()
 					    .unwrap(),
 		    
-		    y_pos: 	cap.get(3).unwrap()
+		    y: 		cap.get(3).unwrap()
 					    .as_str()
 					    .parse::<i32>()
 					    .unwrap(),
 		    
-		    wide: 	cap.get(4).unwrap()
+		    width: 	cap.get(4).unwrap()
 					    .as_str()
 					    .parse::<i32>()
 					    .unwrap(),
@@ -102,17 +104,18 @@ fn main() {
 					    .as_str()
 					    .parse::<i32>()
 					    .unwrap(),
+		    
 		};
 		
 		/* Save the claim data */
 		claim_data.push(tmp)
 	    }
 	}  
-   }
-    
+    }
     
     /* Store details about the claims */
     let mut all_points = HashMap::new();
+    let mut claim_ids = HashSet::new();
     
     /* Iterate over the claims */
     for claim in &claim_data {
@@ -123,9 +126,13 @@ fn main() {
 	for point in &claim_points {
 	    
 	    /* 	If the point doesn't exist create it with count 1
-		otherwise add one to its count. */
-	    all_points.entry(point.clone()).and_modify(|x| *x += 1).or_insert(1);
+	    otherwise add one to its count. */
+	    all_points.entry(point.clone())
+			.and_modify(|x| *x += 1)
+			.or_insert(1);
 	}
+	
+	claim_ids.insert(claim.id);
     }
     
     /* Count the points that overlap */
@@ -135,5 +142,27 @@ fn main() {
 	if *count > 1 {overlaping_points += 1}
     }
     
-    println!("Part 1: the overlaping area is {} inches².", overlaping_points)
+    println!("Part 1: the overlaping area is {} inches².", overlaping_points);   
+    
+    /* Detect overlaps */
+    for claim in &claim_data {
+	
+	let mut overlap = false;
+	
+	/* Extract the points that the claim covers */
+	let claim_points = claim.all_points();
+	
+	for point in &claim_points {
+	    if all_points.get(point) > Some(&1) {
+		
+		overlap = true;
+		break
+	    }
+	}
+	
+	if !overlap {
+	    println!("Part 2: the non-overlaping claim is {}.", claim.id); 
+	    break
+	}	
+    } 
 }
