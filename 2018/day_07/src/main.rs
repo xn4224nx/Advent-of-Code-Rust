14c7776ccec562239c07919a10c5b24421d44705
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-05-19 00:15:46 +0100

    Day 07 - Part 1 - Inital Setup

---
+++ 2018/day_07/src/main.rs
@@ -0,0 +1,25 @@
+/************************************************************************
+*									*
+*	--- Day 7: The Sum of Its Parts ---				*
+*									*
+*	Part 1 - Using the supplied step instructions determine what 	*
+* 		order they should be completed in. 			*
+*									*
+************************************************************************/
+
+
+fn main() {
+    
+    /* Load the instructions from disk. */
+    
+    /* Parse each line into a tuple of chars. */
+    
+    /* Add the chars into a hashmap. */
+    
+    /* Loop over the data structure. */
+    
+    /* Find the step that has no prerequisites.  */
+    
+    /* Iterate over the datastrucutre working out the next steps. */
+    
+}
