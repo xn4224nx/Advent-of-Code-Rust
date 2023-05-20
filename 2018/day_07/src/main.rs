7822e66a17e4f289c258107ec6667e921a7e44eb
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-05-20 23:51:25 +0100

    Day 07 - Part 1 - Read Instructions From File

--- 2018/day_07/src/main.rs
+++ 2018/day_07/src/main.rs
@@ -7,10 +7,40 @@
 *									*
 ************************************************************************/
 
+/* Load and read the file */
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+
+fn load_instructions<P>(filename: P) -> Vec<(char, char)> 
+where P: AsRef<Path>,{
+    /* Load the instructions from file and parse into a vector. */
+    
+    let instructs = vec![];
+    
+    /* Open the instructions file as read only. */
+    let file = File::open(filename).unwrap();
+    
+    /* Load the file and iterate over it */
+    if let lines = io::BufReader::new(file).lines() {
+	    
+	for line in lines {
+	    if let Ok(raw_line) = line {
+		println!("{}", raw_line);
+	    }
+	}
+    }
+	
+    return instructs
+}
+
 
 fn main() {
     
     /* Load the instructions from disk. */
+    let var = load_instructions("./data/sample.txt");
+    
     
     /* Parse each line into a tuple of chars. */
     
