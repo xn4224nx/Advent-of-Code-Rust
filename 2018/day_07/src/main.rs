2fc9a0e11db5da4cad8fb42c10670e376e199165
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-05-29 17:31:23 +0100

    Day 07 - Part 1 - Created Node Order Fn

--- 2018/day_07/src/main.rs
+++ 2018/day_07/src/main.rs
@@ -14,6 +14,7 @@ use std::path::Path;
 
 /* HashMaps */
 use  std::collections::HashMap;
+use std::collections::HashSet;
 
 /* Parse The Data */
 use regex::Regex;
@@ -62,35 +63,84 @@ where P: AsRef<Path>,{
 
 
 
-fn create_lookup(instructions: Vec<(char, char)>) -> HashMap<char, Vec<char>> {
+fn create_lookup(instructions: Vec<(char, char)>) -> 
+    HashMap<char, HashSet<char>> {
     /* Create a HashMap of the required Nodes for each Node */
     
-    let mut req_nodes: HashMap<char, Vec<char>> = HashMap::new();
+    let mut req_nodes: HashMap<char, HashSet<char>> = HashMap::new();
     
     /* Iterate over the vector of instructions */
     for inst in instructions {
 	
 	/* If a key doesn't exist create it. */
-	req_nodes.entry(inst.0).or_insert(vec![]);
+	req_nodes.entry(inst.0).or_insert(HashSet::new());
 	
 	/* Add the requred nodes to the hashmap */
-	req_nodes.entry(inst.1).and_modify(|node| node.push(inst.0))
-				.or_insert(vec![inst.0]);
+	req_nodes.entry(inst.1).and_modify(|node| {node.insert(inst.0);})
+				.or_insert(HashSet::from([inst.0]));
     }
     
     return req_nodes
 }
 
 
-fn find_node_order(node_lookup: HashMap<char, Vec<char>>) -> String {
+fn find_node_order(node_lookup: HashMap<char, HashSet<char>>) -> String {
     /* Create a string that indicates the node order. */
     
-    /* Find the first Node */
     
-    /* Iterate over the Node map and find ones that can be next */
+    /* Copy the contents of the node_lookup into another HashMap */
+    let mut lookup: HashMap<char, HashSet<char>> = HashMap::new();
+    
+    for (key, value) in node_lookup.iter() {
+	lookup.insert(*key, value.clone());
+    }
     
     
-    return String::from("");
+    /* String that will store the answers */
+    let mut node_order = String::from("");
+    
+    
+    /* Iterate over the hash map while the string has less chars than nodes */
+    while lookup.len() > 0 {
+	
+	/* Find nodes that have no requirements */
+	let mut all_next_nodes = vec![];
+	
+	for (key, value) in lookup.iter_mut() {
+	    
+	    /* If the node has no required nodes save it */
+	    if value.len() == 0 {
+		all_next_nodes.push(key.clone());
+	    }
+	}
+	
+	
+	/* Order the next_node vector */
+	all_next_nodes.sort();
+	
+	/* Select the First Node */
+	let next_node = &all_next_nodes[0];
+	
+	/* Remove the next node from  the hashmap */
+	lookup.remove(next_node);
+	
+	/* Remove the next node from the values */
+	for value in lookup.values_mut() {
+
+	    value.remove(next_node);
+	}
+
+
+	/* Add the Letters to the node order */
+	node_order.push(*next_node);
+
+	println!("{:?}", all_next_nodes);
+	println!("{:?}", lookup);
+	println!("{}", node_order);
+
+    }
+    
+    return node_order;
 }
 
 fn main() {
@@ -104,11 +154,7 @@ fn main() {
     
     println!("{:?}", node_dep);
     
-    
-    /* Loop over the data structure. */
-    
-    /* Find the step that has no prerequisites.  */
-    
-    /* Iterate over the datastrucutre working out the next steps. */
+    /* Find the Node  */
+    find_node_order(node_dep);
     
 }
