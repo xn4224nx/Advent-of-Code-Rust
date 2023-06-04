61810ecba5742f657eada39abe0c65dca66c996d
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-06-05 00:44:29 +0100

    Day 08 - Part 1 - Data Read Fn

--- 2018/day_08/src/main.rs
+++ 2018/day_08/src/main.rs
@@ -6,6 +6,32 @@
 *									*	
 ************************************************************************/
 
+use std::path::Path;
+use std::fs::File;
+use std::io::Read;
+
+
+/// Read the licence file and return a vector of the integer values
+fn read_licence_file<P>(filename: P) -> Vec<i32>
+where P: AsRef<Path>{
+    
+    /* Open the file */
+    let mut file = File::open(filename).expect("Unable to open licence.");
+    
+    /* Read the file into a string */
+    let mut buffer = String::new();
+    file.read_to_string(&mut buffer).expect("Unable to read licence.");
+    
+    /* Parse the licence numbers into a Vector */
+    let licence: Vec<i32> = buffer.split_whitespace()
+					    .map(|s| s.parse().unwrap())
+					    .collect();
+    return licence;
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let raw_licence = read_licence_file("./data/sample.txt");
+    
+    println!("{:?}", raw_licence);
 }
