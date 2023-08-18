64d822e54bf00fd24035de94b40c8b9fdf6ee667
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-08-18 22:54:54 +0100

    Read data as a vector of strings.

--- 2018/day_10/src/main.rs
+++ 2018/day_10/src/main.rs
@@ -6,7 +6,32 @@
 *									                                    *
 ************************************************************************/
 
+use std::fs::read_to_string;
+
+
 fn main() {
+
+    /* Read the data into memory. */
+    let raw_lines = read_lines_to_vec("./data/sample.txt");
     
-    /* Read the data into memory */
+    /* Print to screen. */
+    for line in raw_lines {
+    
+        println!("{}", line);
+    }
+    
 }
+
+
+fn read_lines_to_vec(filename: &str) -> Vec<String> {
+    
+    let mut file_lines = Vec::new();
+    
+    /* Read the file into a list. */
+    for line in read_to_string(filename).unwrap().lines() {
+        file_lines.push(line.to_string());
+    }
+    
+    return file_lines;
+}
+
