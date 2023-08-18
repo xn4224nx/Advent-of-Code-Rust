83cbc258b00d4abaf10a9feda75836b4eb7f5fcc
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-08-18 22:23:00 +0100

    Added explanation text.

--- 2018/day_10/src/main.rs
+++ 2018/day_10/src/main.rs
@@ -1,3 +1,12 @@
+/************************************************************************
+*									                                    *
+*	--- Day 10: The Stars Align ---					                    *
+*									                                    *
+*	Part 1 - What message will eventually appear in the sky?            *
+*									                                    *
+************************************************************************/
+
 fn main() {
-    println!("Hello, world!");
+    
+    /* Read the data into memory */
 }
