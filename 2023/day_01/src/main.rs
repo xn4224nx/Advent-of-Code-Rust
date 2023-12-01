5bf3ba274a2cdd569e2edb06f6f9f542f8e87ccc
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-12-01 12:44:01 +0000

    Created a fn.

--- 2023/day_01/src/main.rs
+++ 2023/day_01/src/main.rs
@@ -15,55 +15,51 @@
  *
  * Consider your entire calibration document. What is the sum of all of
  * the calibration values?
- * 
- * Your calculation isn't quite right. It looks like some of the digits 
- * are actually spelled out with letters: one, two, three, four, five, 
+ *
+ * Your calculation isn't quite right. It looks like some of the digits
+ * are actually spelled out with letters: one, two, three, four, five,
  * six, seven, eight, and nine also count as valid "digits".
- * 
- * Equipped with this new information, you now need to find the real 
+ *
+ * Equipped with this new information, you now need to find the real
  * first and last digit on each line.
  */
 
 use regex::Regex;
-use std::fs;
 use std::collections::HashMap;
+use std::fs;
 
+fn calibration_sum(input_file: &str, spelled_digits: bool) -> u32 {
+    let num_replace: HashMap<&str, &str> = HashMap::from([
+        ("zero", "z0o"),
+        ("one", "o1e"),
+        ("two", "t2o"),
+        ("three", "t3e"),
+        ("four", "f4r"),
+        ("five", "f5e"),
+        ("six", "s6x"),
+        ("seven", "s7n"),
+        ("eight", "e8t"),
+        ("nine", "n9e"),
+    ]);
 
-
-fn main() {
-    let source_fp = "../data/input.txt";
-	
-	let num_replace: HashMap<&str, &str> = HashMap::from([
-		("zero", "z0o"),
-		("one", "o1e"),
-		("two", "t2o"),
-		("three", "t3e"),
-		("four", "f4r"),
-		("five", "f5e"),
-		("six", "s6x"),
-		("seven", "s7n"),
-		("eight", "e8t"),
-		("nine", "n9e"),
-	]);
-	
     let mut ex_nums = vec![];
 
     /* Load the raw text file and split it by new lines. */
-    let contents = fs::read_to_string(source_fp).expect("Unable To Read File");
+    let contents = fs::read_to_string(input_file).expect("Unable To Read File");
     let instructs: Vec<_> = contents.split('\n').collect();
 
     /* Iterate over the lines and extract the numbers. */
     let re = Regex::new(r"(\d)").unwrap();
 
     for lin_ist in instructs {
-		
-		let mut rep_line = lin_ist.to_owned();
-		
-        /* Replace the written digits. */
-        for (orig_txt, rep_txt) in num_replace.iter() {
-			rep_line = rep_line.replace(orig_txt, rep_txt);
-		}
-		
+        let mut rep_line = lin_ist.to_owned();
+
+        if spelled_digits {
+            /* Replace the written digits. */
+            for (orig_txt, rep_txt) in num_replace.iter() {
+                rep_line = rep_line.replace(orig_txt, rep_txt);
+            }
+        }
         /* Extract all the digits in the string. */
         let digits: Vec<_> = re.captures_iter(&rep_line).collect();
         if digits.len() <= 0 {
@@ -76,5 +72,18 @@ fn main() {
 
         ex_nums.push(fin_num);
     }
-    println!("Sum of numbers = {}", ex_nums.iter().sum::<u32>());
+
+    return ex_nums.iter().sum::<u32>();
+}
+
+fn main() {
+    println!(
+        "Part 1 : Sum of numbers = {}",
+        calibration_sum("../data/input.txt", false)
+    );
+
+    println!(
+        "Part 2 : Sum of numbers = {}",
+        calibration_sum("../data/input.txt", true)
+    );
 }
