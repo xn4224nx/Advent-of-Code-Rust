b86758651f97a072746c3dba96cd7603e2865adb
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-12-02 05:38:35 +0000

    Setup for day 2.

---
+++ 2023/day_02/src/main.rs
@@ -0,0 +1,24 @@
+/* 
+ * --- Day 2: Cube Conundrum ---
+ *
+ * As you walk, the Elf shows you a small bag and some cubes which are either 
+ * red, green, or blue. Each time you play this game, he will hide a secret 
+ * number of cubes of each color in the bag, and your goal is to figure out 
+ * information about the number of cubes.
+ *
+ * To get information, once a bag has been loaded with cubes, the Elf will reach
+ * into the bag, grab a handful of random cubes, show them to you, and then put 
+ * them back in the bag. He'll do this a few times per game.
+ *
+ * The Elf would first like to know which games would have been possible if the 
+ * bag contained only 12 red cubes, 13 green cubes, and 14 blue cubes?
+ *
+ * Determine which games would have been possible if the bag had been loaded 
+ * with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of
+ * the IDs of those games?
+ */
+
+
+fn main() {
+    
+}
