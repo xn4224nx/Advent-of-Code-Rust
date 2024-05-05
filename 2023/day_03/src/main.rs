889ba7656503b8489d98e170f8e310880d9d95e2
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-05-06 00:31:48 +0100

    Answer to part 1.

--- 2023/day_03/src/main.rs
+++ 2023/day_03/src/main.rs
@@ -15,6 +15,7 @@
  * Part 1: What is the sum of all of the part numbers in the engine schematic?
  */
 
+use regex::Regex;
 use std::fs;
 
 /// Load the data into a vector of strings.
@@ -26,9 +27,99 @@ fn file_to_str_vec(file_pth: &str) -> Vec<String> {
         .collect();
 }
 
+/// For a line extract the number and its start position.
+fn extract_num_pos(raw_line: &String) -> Vec<(u32, u32)> {
+    let re_pat = Regex::new(r"[0-9]+").unwrap();
+    let mut numbers_positions = Vec::new();
 
+    for reg_res in re_pat.find_iter(&raw_line) {
+        let num = reg_res.as_str().parse::<u32>().unwrap();
+        let pos = reg_res.start() as u32;
+        numbers_positions.push((num, pos));
+    }
+
+    return numbers_positions;
+}
+
+/// Do two points in a grid overlap?
+fn coord_overlap(pnt_0: (u32, u32), pnt_1: (u32, u32)) -> bool {
+    let x_diff = pnt_0.0 as i32 - pnt_1.0 as i32;
+    let y_diff = pnt_0.1 as i32 - pnt_1.1 as i32;
+
+    return if x_diff.abs() < 2 && y_diff.abs() < 2 {
+        true
+    } else {
+        false
+    };
+}
+
+/// Get all the coordinates covered by a number
+fn number_coverage(num_pos: (u32, u32), row: u32) -> Vec<(u32, u32)> {
+    let mut number_coverage = Vec::new();
+    let num_digits = num_pos.0.checked_ilog10().unwrap_or(0) + 1;
+    let pos = num_pos.1;
+
+    for n in 0..num_digits {
+        number_coverage.push((pos + n, row))
+    }
+
+    return number_coverage;
+}
+
+/// Take the grid of positons and numbers and determine the sum of the numbers
+/// in the engine schematic.
+fn part_num_sum(raw_data: &Vec<String>, grid: &Vec<Vec<(u32, u32)>>) -> u32 {
+    let re_pat = Regex::new(r#"[!#$%&'()*/+/,\-:@/[/]^_{|}~\?\\=]"#).unwrap();
+    let mut sum = 0;
+
+    for (row_idx, line) in raw_data.iter().enumerate() {
+        /* Find the symbol positions */
+        let sym_pos: Vec<u32> = re_pat.find_iter(&line).map(|x| x.start() as u32).collect();
+
+        for col_idx in &sym_pos {
+            /* Check for the numbers in-line. */
+            for num in &grid[row_idx] {
+                for point in number_coverage(*num, row_idx as u32) {
+                    if coord_overlap(point, (*col_idx, row_idx as u32)) {
+                        sum += num.0;
+                        break;
+                    }
+                }
+            }
+
+            /* Check for numbers above. */
+            if row_idx != 0 {
+                for num in &grid[row_idx - 1] {
+                    for point in number_coverage(*num, row_idx as u32) {
+                        if coord_overlap(point, (*col_idx, row_idx as u32)) {
+                            sum += num.0;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            /* Check for numbers below. */
+            if row_idx != grid.len() - 1 {
+                for num in &grid[row_idx + 1] {
+                    for point in number_coverage(*num, row_idx as u32) {
+                        if coord_overlap(point, (*col_idx, row_idx as u32)) {
+                            sum += num.0;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return sum;
+}
 
 fn main() {
-    let data = file_to_str_vec("./data/example_0.txt");
-    println!("{:?}", data);
+    let data = file_to_str_vec("./data/input.txt");
+    let data_postions: Vec<_> = data.iter().map(|x| extract_num_pos(&x)).collect();
+    let p1_ans = part_num_sum(&data, &data_postions);
+
+    println!("The answer to part one = {}", p1_ans);
 }
