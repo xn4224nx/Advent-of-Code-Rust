1782db6beccb0d58a84359b61aaf02801e831a21
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-05-05 17:10:58 +0100

    Day 3 setup.

---
+++ 2023/day_03/src/main.rs
@@ -0,0 +1,20 @@
+/*
+ * --- Day 3: Gear Ratios ---
+ *
+ * The engineer explains that an engine part seems to be missing from the 
+ * engine, but nobody can figure out which one. If you can add up all the part 
+ * numbers in the engine schematic, it should be easy to work out which part is 
+ * missing.
+ *
+ * The engine schematic (your puzzle input) consists of a visual representation 
+ * of the engine. There are lots of numbers and symbols you don't really 
+ * understand, but apparently any number adjacent to a symbol, even diagonally, 
+ * is a "part number" and should be included in your sum. (Periods (.) do not 
+ * count as a symbol.)
+ *
+ * Part 1: What is the sum of all of the part numbers in the engine schematic?
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
