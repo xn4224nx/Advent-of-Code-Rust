f2e92bc08c3f6fe8f2f9e9521499b875761a8e5d
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-05-07 23:05:40 +0100

    Setup for day 4.

---
+++ 2023/day_04/src/main.rs
@@ -0,0 +1,17 @@
+/*
+* --- Day 4: Scratchcards ---
+*
+* It looks like each card has two lists of numbers separated by a vertical bar
+* (|): a list of winning numbers and then a list of numbers you have.
+*
+* As far as the Elf has been able to figure out, you have to figure out which
+* of the numbers you have appear in the list of winning numbers. The first match
+* makes the card worth one point and each match after the first doubles the
+* point value of that card.
+*
+* Part 1: How many points are they worth in total?
+*/
+
+fn main() {
+    println!("Hello, world!");
+}
