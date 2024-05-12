3c06dacc803fe71bdf70b145363f8b38a76c4a44
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-05-12 21:43:39 +0100

    Setup for day four's problem.

---
+++ 2023/day_05/src/main.rs
@@ -0,0 +1,30 @@
+/*
+* --- Day 4: Scratchcards ---
+*
+* The almanac (your puzzle input) lists all of the seeds that need to be 
+* planted. It also lists what type of soil to use with each kind of seed, what 
+* type of fertilizer to use with each kind of soil, what type of water to use 
+* with each kind of fertilizer, and so on. Every type of seed, soil, fertilizer 
+* and so on is identified with a number, but numbers are reused by each category
+* - that is, soil 123 and fertilizer 123 aren't necessarily related to each 
+* other.
+*
+* The almanac starts by listing which seeds need to be planted. The rest of the 
+* almanac contains a list of maps which describe how to convert numbers from a 
+* source category into numbers in a destination category. That is, the section 
+* that starts with seed-to-soil map: describes how to convert a seed number (the
+* source) to a soil number (the destination).
+*
+* Rather than list every source number and its corresponding destination number 
+* one by one, the maps describe entire ranges of numbers that can be converted. 
+* Each line within a map contains three numbers: the destination range start, 
+* the source range start, and the range length.
+*
+* Any source numbers that aren't mapped correspond to the same destination 
+* number. So, seed number 10 corresponds to soil number 10.
+*
+* Part 1 - What is the lowest location number that corresponds to any of the 
+*          initial seed numbers?
+*/
+
+fn main() {}
