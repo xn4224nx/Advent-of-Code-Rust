237ff12966ecbe2c3d264fac6455b78fdc33f707
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-05-22 20:56:33 +0100

    Setup for day 6.

---
+++ 2023/day_06/src/main.rs
@@ -0,0 +1,28 @@
+/*
+* --- Day 6: Wait For It ----
+*
+* You get a sheet of paper (your puzzle input) that lists the time allowed for
+* each race and also the best distance ever recorded in that race. To guarantee
+* you win the grand prize, you need to make sure you go farther in each race
+* than the current record holder.
+*
+* Holding down the button charges the boat, and releasing the button allows the
+* boat to move. Boats move faster if their button was held longer, but time
+* spent holding the button counts against the total race time. You can only hold
+* the button at the start of the race, and boats don't move until the button is
+* released.
+*
+* Your toy boat has a starting speed of zero millimeters per millisecond. For
+* each whole millisecond you spend at the beginning of the race holding down the
+* button, the boat's speed increases by one millimeter per millisecond.
+*
+* To see how much margin of error you have, determine the number of ways you can
+* beat the record in each race.
+*
+* Part 1 - Determine the number of ways you could beat the record in each race.
+*          What do you get if you multiply these numbers together?
+*/
+
+fn main() {
+    println!("Hello, world!");
+}
