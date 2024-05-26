d593444b405a7e3053cfeb083e1aca167dc892e3
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-05-26 18:10:13 +0100

    Data ingestion for day 6.

--- 2023/day_06/src/main.rs
+++ 2023/day_06/src/main.rs
@@ -23,6 +23,41 @@
 *          What do you get if you multiply these numbers together?
 */
 
+use regex::Regex;
+use std::fs;
+use std::iter::zip;
+
+/// Read the race file and create a vector of the time and distances
+fn read_races(file_path: &str) -> Vec<(u32, u32)> {
+    let num_re = Regex::new(r"[\d]+").unwrap();
+    let mut extracted_nums: Vec<Vec<u32>> = Vec::new();
+
+    /* Read the file as a string. */
+    let raw_file_contents =
+        fs::read_to_string(file_path).expect("File '{file_path}' could not be read!");
+
+    /* Parse the data in the string by iteration over all the lines. */
+    for raw_line in raw_file_contents.lines() {
+        /* Extract the numbers in the row. */
+        extracted_nums.push(
+            num_re
+                .find_iter(raw_line)
+                .filter_map(|x| x.as_str().parse::<u32>().ok())
+                .collect(),
+        );
+    }
+
+    /* Extract the distance and time into matching tuples. */
+    let ret = zip(&extracted_nums[0], &extracted_nums[1])
+        .map(|(x, y)| (*x, *y))
+        .collect::<Vec<(u32, u32)>>();
+
+    return ret;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let example = read_races("./data/example.txt");
+    let input = read_races("./data/input.txt");
+
+    println!("\nexample races: {:?}\ninput races: {:?}\n", example, input);
 }
