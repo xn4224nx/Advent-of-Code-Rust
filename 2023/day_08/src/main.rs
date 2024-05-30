8b9ead472becb8f4832e1e670f406590eaeb1cf5
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-05-31 00:29:30 +0100

    Setup for day 8.

---
+++ 2023/day_08/src/main.rs
@@ -0,0 +1,29 @@
+/*
+* --- Day 8: Haunted Wasteland ---
+*
+* One of the camel's pouches is labeled "maps" - sure enough, it's full of
+* documents (your puzzle input) about how to navigate the desert. At least,
+* you're pretty sure that's what they are; one of the documents contains a list
+* of left/right instructions, and the rest of the documents seem to describe
+* some kind of network of labeled nodes.
+*
+* It seems like you're meant to use the left/right instructions to navigate the
+* network. Perhaps if you have the camel follow the same instructions, you can
+* escape the haunted wasteland!
+*
+* After examining the maps for a bit, two nodes stick out: AAA and ZZZ. You feel
+* like AAA is where you are now, and you have to follow the left/right
+* instructions until you reach ZZZ.
+*
+* Starting with AAA, you need to look up the next element based on the next
+* left/right instruction in your input. Of course, you might not find ZZZ right
+* away. If you run out of left/right instructions, repeat the whole sequence of
+* instructions as necessary.
+*
+* Part 1 - Starting at AAA, follow the left/right instructions. How many steps
+*          are required to reach ZZZ?
+*/
+
+fn main() {
+    println!("Hello, world!");
+}
