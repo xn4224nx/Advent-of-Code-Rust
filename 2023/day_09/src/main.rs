2e9b37ef53718390fd7f91e12cb30871178427f9
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-06-03 02:26:42 +0100

    Setup for day 9.

---
+++ 2023/day_09/src/main.rs
@@ -0,0 +1,22 @@
+/*
+ * --- Day 9: Mirage Maintenance ---
+ *
+ * The OASIS produces a report of many values and how they are changing
+ * over time (your puzzle input). Each line in the report contains the
+ * history of a single value.
+ *
+ * To best protect the oasis, your environmental report should include a
+ * prediction of the next value in each history. To do this, start by
+ * making a new sequence from the difference at each step of your
+ * history. If that sequence is not all zeroes, repeat this process,
+ * using the sequence you just generated as the input sequence. Once all
+ * of the values in your latest sequence are zeroes, you can extrapolate
+ * what the next value of the original history should be.
+ *
+ * Part 1 - Analyze your OASIS report and extrapolate the next value for
+ *          each history. What is the sum of these extrapolated values?
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
