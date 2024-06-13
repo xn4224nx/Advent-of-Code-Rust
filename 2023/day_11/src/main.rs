d35aaaac2347f9d676861f51109a16b92b47329c
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-06-13 21:31:39 +0100

    Setup for day 11.

---
+++ 2023/day_11/src/main.rs
@@ -0,0 +1,30 @@
+/*
+ * --- Day 11: Cosmic Expansion ---
+ * 
+ * The researcher has collected a bunch of data and compiled the data 
+ * into a single giant image (your puzzle input). The image includes 
+ * empty space (.) and galaxies (#).
+ * 
+ * The researcher is trying to figure out the sum of the lengths of the 
+ * shortest path between every pair of galaxies. However, there's a 
+ * catch: the universe expanded in the time it took the light from those
+ * galaxies to reach the observatory.
+ * 
+ * Due to something involving gravitational effects, only some space 
+ * expands. In fact, the result is that any rows or columns that contain
+ * no galaxies should all actually be twice as big.
+ * 
+ * Only count each pair once; order within the pair doesn't matter. For 
+ * each pair, find any shortest path between the two galaxies using only
+ * steps that move up, down, left, or right exactly one . or # at a 
+ * time. (The shortest path between two galaxies is allowed to pass 
+ * through another galaxy.)
+ * 
+ * Part 1 - Expand the universe, then find the length of the shortest 
+ *          path between every pair of galaxies. What is the sum of 
+ *          these lengths?
+ */ 
+
+fn main() {
+    println!("Hello, world!");
+}
