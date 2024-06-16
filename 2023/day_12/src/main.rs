9c1671709ead55e47e3689d0bc310f6c958ba5a5
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-06-16 17:18:48 +0100

    Setup for day 12.

---
+++ 2023/day_12/src/main.rs
@@ -0,0 +1,36 @@
+/*
+ * --- Day 12: Hot Springs ---
+ *
+ * There's just one problem - many of the springs have fallen into
+ * disrepair, so they're not actually sure which springs would even be
+ * safe to use! Worse yet, their condition records of which springs are
+ * damaged (your puzzle input) are also damaged! You'll need to help
+ * them repair the damaged records.
+ *
+ * In the giant field just outside, the springs are arranged into rows.
+ * For each row, the condition records show every spring and whether it
+ * is operational (.) or damaged (#). This is the part of the condition
+ * records that is itself damaged; for some springs, it is simply
+ * unknown (?) whether the spring is operational or damaged.
+ *
+ * However, the engineer that produced the condition records also
+ * duplicated some of this information in a different format! After the
+ * list of springs for a given row, the size of each contiguous group of
+ * damaged springs is listed in the order those groups appear in the
+ * row. This list always accounts for every damaged spring, and each
+ * number is the entire size of its contiguous group (that is, groups
+ * are always separated by at least one operational spring: #### would
+ * always be 4, never 2,2).
+ *
+ * Equipped with this information, it is your job to figure out how many
+ * different arrangements of operational and broken springs fit the
+ * given criteria in each row.
+ *
+ * Part 1 - For each row, count all of the different arrangements of
+ *          operational and broken springs that meet the given criteria.
+ *          What is the sum of those counts?
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
