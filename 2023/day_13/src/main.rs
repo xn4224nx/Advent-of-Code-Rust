65e84cb6c9fbff635100b2733f08904d0ac2b944
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-06-19 17:30:18 +0100

    Setup for day 13.

---
+++ 2023/day_13/src/main.rs
@@ -0,0 +1,36 @@
+/*
+ * --- Day 13: Point of Incidence ---
+ * 
+ * After a while, you make your way to a nearby cluster of mountains 
+ * only to discover that the valley between them is completely full of 
+ * large mirrors. Most of the mirrors seem to be aligned in a consistent
+ * way; perhaps you should head in that direction?
+ * 
+ * As you move through the valley of mirrors, you find that several of 
+ * them have fallen from the large metal frames keeping them in place. 
+ * The mirrors are extremely flat and shiny, and many of the fallen 
+ * mirrors have lodged into the ash at strange angles. Because the 
+ * terrain is all one color, it's hard to tell where it's safe to walk 
+ * or where you're about to run into a mirror.
+ * 
+ * You note down the patterns of ash (.) and rocks (#) that you see as 
+ * you walk (your puzzle input); perhaps by carefully analyzing these 
+ * patterns, you can figure out where the mirrors are!
+ * 
+ * To find the reflection in each pattern, you need to find a perfect 
+ * reflection across either a horizontal line between two rows or across
+ * a vertical line between two columns.
+ * 
+ * To summarize your pattern notes, add up the number of columns to the 
+ * left of each vertical line of reflection; to that, also add 100 
+ * multiplied by the number of rows above each horizontal line of 
+ * reflection.
+ * 
+ * Part 1 - Find the line of reflection in each of the patterns in your 
+ *          notes. What number do you get after summarizing all of your 
+ *          notes?
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
