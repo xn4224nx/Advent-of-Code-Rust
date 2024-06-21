aba131abae8feca95efd6059b204e14bde5bb8dd
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-06-21 21:38:57 +0100

    Setup for day 14.

---
+++ 2023/day_14/src/main.rs
@@ -0,0 +1,32 @@
+/*
+ * --- Day 14: Parabolic Reflector Dish ---
+ *
+ * The platform is covered in large rocks of various shapes. Depending 
+ * on their position, the weight of the rocks deforms the platform, and 
+ * the shape of the platform controls which ropes move and ultimately 
+ * the focus of the dish.
+ * 
+ * In short: if you move the rocks, you can focus the dish. The platform
+ * even has a control panel on the side that lets you tilt it in one of 
+ * four directions! The rounded rocks (O) will roll when the platform is
+ * tilted, while the cube-shaped rocks (#) will stay in place. You note 
+ * the positions of all of the empty spaces (.) and rocks (your puzzle 
+ * input).
+ * 
+ * You notice that the support beams along the north side of the 
+ * platform are damaged; to ensure the platform doesn't collapse, you 
+ * should calculate the total load on the north support beams.
+ * 
+ * The amount of load caused by a single rounded rock (O) is equal to 
+ * the number of rows from the rock to the south edge of the platform, 
+ * including the row the rock is on. (Cube-shaped rocks (#) don't 
+ * contribute to load.)
+ * 
+ * Part 1 - Tilt the platform so that the rounded rocks all roll north. 
+ *          Afterward, what is the total load on the north support 
+ *          beams?
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
