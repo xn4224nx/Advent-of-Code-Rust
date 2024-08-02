593d0d3d2db4c888bddecb1660e044ae622d9c37
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-08-03 00:11:54 +0100

    Setup for day 3.

---
+++ 2015/Day_03/src/main.rs
@@ -0,0 +1,23 @@
+/*
+ * --- Day 3: Perfectly Spherical Houses in a Vacuum ---
+ *
+ * Santa is delivering presents to an infinite two-dimensional
+ * grid of houses.
+ *
+ * He begins by delivering a present to the house at his
+ * starting location, and then an elf at the North Pole calls
+ * him via radio and tells him where to move next. Moves are
+ * always exactly one house to the north (^), south (v), east
+ * (>), or west (<). After each move, he delivers another
+ * present to the house at his new location.
+ *
+ * However, the elf back at the north pole has had a little too
+ * much eggnog, and so his directions are a little off, and
+ * Santa ends up visiting some houses more than once.
+ *
+ * PART 1: How many houses receive at least one present?
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
