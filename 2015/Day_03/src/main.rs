37a0fc31fe0a5db8e9f5024f0a137aacbf97a874
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-08-04 16:01:56 +0100

    Part 1 completed.

--- 2015/Day_03/src/main.rs
+++ 2015/Day_03/src/main.rs
@@ -18,6 +18,64 @@
  * PART 1: How many houses receive at least one present?
  */
 
+use std::collections::HashSet;
+use std::fs;
+
+#[derive(PartialEq, Debug)]
+pub enum GridDir {
+    North,
+    South,
+    West,
+    East,
+}
+
+/// Read a text file and parse into grid directions
+pub fn read_directions(file_path: &str) -> Vec<GridDir> {
+    let mut parsed_dirs = Vec::new();
+
+    /* Open the file and put everything into a String. */
+    let file_contents = fs::read_to_string(file_path).unwrap();
+
+    /* Iterate over every char and parse the direction */
+    for f_char in file_contents.chars() {
+        match f_char {
+            '^' => parsed_dirs.push(GridDir::North),
+            'v' => parsed_dirs.push(GridDir::South),
+            '<' => parsed_dirs.push(GridDir::West),
+            '>' => parsed_dirs.push(GridDir::East),
+            _ => continue,
+        }
+    }
+
+    return parsed_dirs;
+}
+
+/// Count the number of houses visited by Santa at least
+/// once.
+pub fn count_visited_houses(directions: &Vec<GridDir>) -> usize {
+    let mut c_loc = (0, 0);
+    let mut visited_houses = HashSet::new();
+
+    /* The starting house is always visited. */
+    visited_houses.insert(c_loc);
+
+    /* For each direction move the location then save it*/
+    for dir in directions.iter() {
+        match dir {
+            GridDir::North => c_loc = (c_loc.0, c_loc.1 + 1),
+            GridDir::South => c_loc = (c_loc.0, c_loc.1 - 1),
+            GridDir::West => c_loc = (c_loc.0 - 1, c_loc.1),
+            GridDir::East => c_loc = (c_loc.0 + 1, c_loc.1),
+        };
+
+        visited_houses.insert(c_loc);
+    }
+
+    /* Only the unique number of houses visited matters. */
+    return visited_houses.len();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let dirs = read_directions("./data/input.txt");
+    println!("The answer to part 1 = {}", count_visited_houses(&dirs));
 }
---
+++ 2015/Day_03/tests/main_tests.rs
@@ -0,0 +1,57 @@
+#[allow(dead_code)]
+#[allow(unused_variables)]
+#[path = "../src/main.rs"]
+mod day_03;
+use day_03::GridDir::{East, North, South, West};
+
+#[test]
+fn read_example_01() {
+    assert_eq!(day_03::read_directions("./data/example_01.txt"), vec![East])
+}
+
+#[test]
+fn read_example_02() {
+    assert_eq!(
+        day_03::read_directions("./data/example_02.txt"),
+        vec![North, East, South, West]
+    )
+}
+
+#[test]
+fn read_example_03() {
+    assert_eq!(
+        day_03::read_directions("./data/example_03.txt"),
+        vec![North, South, North, South, North, South, North, South, North, South]
+    )
+}
+
+#[test]
+fn read_example_04() {
+    assert_eq!(
+        day_03::read_directions("./data/example_04.txt"),
+        vec![North, South]
+    )
+}
+
+#[test]
+fn unique_visits_ex_01() {
+    assert_eq!(day_03::count_visited_houses(&vec![East]), 2)
+}
+
+#[test]
+fn unique_visits_ex_02() {
+    assert_eq!(
+        day_03::count_visited_houses(&vec![North, East, South, West]),
+        4
+    )
+}
+
+#[test]
+fn unique_visits_ex_03() {
+    assert_eq!(
+        day_03::count_visited_houses(&vec![
+            North, South, North, South, North, South, North, South, North, South
+        ]),
+        2
+    )
+}
