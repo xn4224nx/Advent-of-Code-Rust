6b7e47360a2e225d1f78e88d485093e06906f470
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-08-18 21:50:49 +0100

    Setup for day 8.

---
+++ 2015/Day_08/src/main.rs
@@ -0,0 +1,28 @@
+/*
+ * --- Day 8: Matchsticks ---
+ *
+ * Space on the sleigh is limited this year, and so Santa will be bringing his
+ * list as a digital copy. He needs to know how much space it will take up when
+ * stored.
+ *
+ * It is common in many programming languages to provide a way to escape special
+ * characters in strings. For example, C, JavaScript, Perl, Python, and even PHP
+ * handle special characters in very similar ways.
+ *
+ * However, it is important to realize the difference between the number of
+ * characters in the code representation of the string literal and the number of
+ * characters in the in-memory string itself.
+ *
+ * Santa's list is a file that contains many double-quoted string literals, one
+ * on each line. The only escape sequences used are \\ (which represents a
+ * single backslash), \" (which represents a lone double-quote character), and
+ * \x plus two hexadecimal characters (which represents a single character with
+ * that ASCII code).
+ *
+ * PART 1:  Disregarding the whitespace in the file, what is the number of
+ *          characters of code for string literals minus the number of
+ *          characters in memory for the values of the strings in total for the
+ *          entire file?
+ */
+
+fn main() {}
