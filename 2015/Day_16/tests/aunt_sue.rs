775693a2bcd864110369827483c474ea2844483d
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-08-31 14:33:25 +0100

    Setup for day 16.

---
+++ 2015/Day_16/tests/aunt_sue.rs
@@ -0,0 +1,162 @@
+#[allow(dead_code)]
+#[allow(unused_variables)]
+#[path = "../src/main.rs"]
+mod day_16;
+use day_16::{could_aunt_match, find_true_aunt_index, read_aunt_data};
+
+use std::collections::HashMap;
+
+#[test]
+fn read_cookie_data_ex_01() {
+    assert_eq!(
+        read_aunt_data("./data/p1_aunt.txt"),
+        vec![HashMap::from([
+            (String::from("children"), 3),
+            (String::from("cats"), 7),
+            (String::from("samoyeds"), 2),
+            (String::from("pomeranians"), 3),
+            (String::from("akitas"), 0),
+            (String::from("vizslas"), 0),
+            (String::from("goldfish"), 5),
+            (String::from("trees"), 3),
+            (String::from("cars"), 2),
+            (String::from("perfumes"), 1)
+        ])]
+    );
+}
+
+#[test]
+fn read_cookie_data_ex_02() {
+    assert_eq!(
+        read_aunt_data("./data/p1_example_aunts.txt"),
+        vec![
+            HashMap::from([
+                (String::from("children"), 1),
+                (String::from("vizslas"), 7),
+                (String::from("cars"), 8),
+            ]),
+            HashMap::from([
+                (String::from("children"), 5),
+                (String::from("akitas"), 10),
+                (String::from("perfumes"), 10),
+            ]),
+            HashMap::from([
+                (String::from("pomeranians"), 4),
+                (String::from("vizslas"), 1),
+                (String::from("cars"), 5),
+            ]),
+            HashMap::from([
+                (String::from("children"), 8),
+                (String::from("goldfish"), 5),
+                (String::from("perfumes"), 3)
+            ]),
+            HashMap::from([
+                (String::from("akitas"), 0),
+                (String::from("vizslas"), 0),
+                (String::from("perfumes"), 1)
+            ]),
+            HashMap::from([
+                (String::from("akitas"), 1),
+                (String::from("vizslas"), 0),
+                (String::from("perfumes"), 2)
+            ]),
+            HashMap::from([
+                (String::from("goldfish"), 10),
+                (String::from("cars"), 4),
+                (String::from("perfumes"), 8)
+            ]),
+            HashMap::from([
+                (String::from("children"), 2),
+                (String::from("cats"), 1),
+                (String::from("perfumes"), 7)
+            ]),
+            HashMap::from([
+                (String::from("pomeranians"), 3),
+                (String::from("goldfish"), 10),
+                (String::from("trees"), 10),
+            ]),
+            HashMap::from([
+                (String::from("pomeranians"), 4),
+                (String::from("akitas"), 7),
+                (String::from("trees"), 8),
+            ]),
+        ]
+    );
+}
+
+#[test]
+fn could_aunt_match_01() {
+    let true_aunt = &read_aunt_data("./data/p1_aunt.txt")[0];
+    let pos_aunt = &read_aunt_data("./data/p1_example_aunts.txt")[0];
+    assert_eq!(could_aunt_match(true_aunt, pos_aunt), false);
+}
+
+#[test]
+fn could_aunt_match_02() {
+    let true_aunt = &read_aunt_data("./data/p1_aunt.txt")[0];
+    let pos_aunt = &read_aunt_data("./data/p1_example_aunts.txt")[1];
+    assert_eq!(could_aunt_match(true_aunt, pos_aunt), false);
+}
+
+#[test]
+fn could_aunt_match_03() {
+    let true_aunt = &read_aunt_data("./data/p1_aunt.txt")[0];
+    let pos_aunt = &read_aunt_data("./data/p1_example_aunts.txt")[2];
+    assert_eq!(could_aunt_match(true_aunt, pos_aunt), false);
+}
+
+#[test]
+fn could_aunt_match_04() {
+    let true_aunt = &read_aunt_data("./data/p1_aunt.txt")[0];
+    let pos_aunt = &read_aunt_data("./data/p1_example_aunts.txt")[3];
+    assert_eq!(could_aunt_match(true_aunt, pos_aunt), false);
+}
+
+#[test]
+fn could_aunt_match_05() {
+    let true_aunt = &read_aunt_data("./data/p1_aunt.txt")[0];
+    let pos_aunt = &read_aunt_data("./data/p1_example_aunts.txt")[4];
+    assert_eq!(could_aunt_match(true_aunt, pos_aunt), true);
+}
+
+#[test]
+fn could_aunt_match_06() {
+    let true_aunt = &read_aunt_data("./data/p1_aunt.txt")[0];
+    let pos_aunt = &read_aunt_data("./data/p1_example_aunts.txt")[5];
+    assert_eq!(could_aunt_match(true_aunt, pos_aunt), false);
+}
+
+#[test]
+fn could_aunt_match_07() {
+    let true_aunt = &read_aunt_data("./data/p1_aunt.txt")[0];
+    let pos_aunt = &read_aunt_data("./data/p1_example_aunts.txt")[6];
+    assert_eq!(could_aunt_match(true_aunt, pos_aunt), false);
+}
+
+#[test]
+fn could_aunt_match_08() {
+    let true_aunt = &read_aunt_data("./data/p1_aunt.txt")[0];
+    let pos_aunt = &read_aunt_data("./data/p1_example_aunts.txt")[7];
+    assert_eq!(could_aunt_match(true_aunt, pos_aunt), false);
+}
+
+#[test]
+fn could_aunt_match_09() {
+    let true_aunt = &read_aunt_data("./data/p1_aunt.txt")[0];
+    let pos_aunt = &read_aunt_data("./data/p1_example_aunts.txt")[8];
+    assert_eq!(could_aunt_match(true_aunt, pos_aunt), false);
+}
+
+#[test]
+fn could_aunt_match_10() {
+    let true_aunt = &read_aunt_data("./data/p1_aunt.txt")[0];
+    let pos_aunt = &read_aunt_data("./data/p1_example_aunts.txt")[9];
+    assert_eq!(could_aunt_match(true_aunt, pos_aunt), false);
+}
+
+#[test]
+fn find_true_aunt_index_exp() {
+    let true_aunt = &read_aunt_data("./data/p1_aunt.txt")[0];
+    let all_aunts = &read_aunt_data("./data/p1_example_aunts.txt");
+    assert_eq!(find_true_aunt_index(true_aunt, all_aunts), 5);
+}
