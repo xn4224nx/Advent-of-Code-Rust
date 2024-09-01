e08bbd95618c0762061b32c07e48fc2f3bbaf19a
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-09-01 12:02:47 +0100

    Setup for day 17.

---
+++ 2015/Day_17/src/main.rs
@@ -0,0 +1,28 @@
+/*
+ * --- Day 17: No Such Thing as Too Much ---
+ *
+ * The elves bought too much eggnog again - 150 liters this time. To fit it all
+ * into your refrigerator, you'll need to move it into smaller containers. You
+ * take an inventory of the capacities of the available containers.
+ *
+ * PART 1:  Filling all containers entirely, how many different combinations of
+ *          containers can exactly fit all 150 liters of eggnog?
+ */
+
+/// Read the container sizes and return a vector of the sizes
+pub fn read_container_sizes(container_file: &str) -> Vec<u32> {
+    Vec::new()
+}
+
+/// Ensure that a set of continers can contain the supplied volume of eggnog
+pub fn does_comb_fit(cont_comb: &Vec<u32>, eggnog_vol: u32) -> bool {
+    true
+}
+
+/// Test every combination of container and count the number of combinations
+/// that can exactly fit the volume of eggnog
+pub fn count_cont_combs(continers: &Vec<u32>, eggnog_vol: u32) -> u32 {
+    0
+}
+
+fn main() {}
---
+++ 2015/Day_17/tests/testing_main.rs
@@ -0,0 +1,48 @@
+#[allow(dead_code)]
+#[allow(unused_variables)]
+#[path = "../src/main.rs"]
+mod day_17;
+use day_17::{count_cont_combs, does_comb_fit, read_container_sizes};
+
+#[test]
+fn read_container_sizes_ex1() {
+    assert_eq!(
+        read_container_sizes("./data/example_01.txt"),
+        vec![20, 15, 10, 5, 5]
+    );
+}
+
+#[test]
+fn does_comb_fit_ex1() {
+    assert_eq!(does_comb_fit(&vec![15, 10], 25), true);
+}
+
+#[test]
+fn does_comb_fit_ex2() {
+    assert_eq!(does_comb_fit(&vec![20, 5], 25), true);
+}
+
+#[test]
+fn does_comb_fit_ex3() {
+    assert_eq!(does_comb_fit(&vec![15, 5, 5], 25), true);
+}
+
+#[test]
+fn does_comb_fit_ex4() {
+    assert_eq!(does_comb_fit(&vec![15, 10, 5], 25), false);
+}
+
+#[test]
+fn does_comb_fit_ex5() {
+    assert_eq!(does_comb_fit(&vec![20], 25), false);
+}
+
+#[test]
+fn does_comb_fit_ex6() {
+    assert_eq!(does_comb_fit(&vec![20, 5, 5], 25), false);
+}
+
+#[test]
+fn count_cont_combs_ex1() {
+    assert_eq!(count_cont_combs(&vec![20, 15, 10, 5, 5], 25), 4);
+}
