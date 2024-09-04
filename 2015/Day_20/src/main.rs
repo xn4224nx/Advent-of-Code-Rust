b8c36ba37eb1f8ac19c8f9d9c632992bef7183c8
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-09-04 23:57:54 +0100

    Setup for day 20.

---
+++ 2015/Day_20/src/main.rs
@@ -0,0 +1,29 @@
+/*
+ * --- Day 20: Infinite Elves and Infinite Houses ---
+ *
+ * To keep the Elves busy, Santa has them deliver some presents by hand, door-
+ * to-door. He sends them down a street with infinite houses numbered
+ * sequentially: 1, 2, 3, 4, 5, and so on.
+ *
+ * Each Elf is assigned a number, too, and delivers presents to houses based on
+ * that number:
+ *
+ *  -   The first Elf (number 1) delivers presents to every house: 1, 2, 3, 4,
+ *      5, ....
+ *
+ *  -   The second Elf (number 2) delivers presents to every second house: 2,
+ *      4, 6, 8, 10, ....
+ *
+ *  -   Elf number 3 delivers presents to every third house: 3, 6, 9, 12, 15,
+ *      ....
+ *
+ * There are infinitely many Elves, numbered starting with 1. Each Elf delivers
+ * presents equal to ten times his or her number at each house.
+ *
+ * PART 1:  What is the lowest house number of the house to get at least as
+ *          many presents as the number in your puzzle input, 34000000?
+ */
+
+use ndarray::{s, Array1};
+
+fn main() {}
