96a4904877fb2037f5d2256dca0fcb8bcf187c09
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-09-18 22:10:31 +0100

    Data ingestion function.

--- 2015/Day_21/src/main.rs
+++ 2015/Day_21/src/main.rs
@@ -55,9 +55,12 @@
  *          the fight?
  */
 
+use regex::Regex;
 use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
-#[derive(Hash, Eq, PartialEq, Debug)]
+#[derive(Clone, Hash, Eq, PartialEq, Debug)]
 pub enum Item {
     Weapon,
     Armour,
@@ -82,7 +85,58 @@ pub struct Stats {
 
 /// Parse the shop data and convert it into a hashmap format.
 pub fn read_shop_data(file_path: &str) -> HashMap<Item, Vec<ShopItem>> {
-    HashMap::new()
+    let mut shop = HashMap::from([
+        (Item::Weapon, Vec::new()),
+        (Item::Armour, Vec::new()),
+        (Item::Ring, Vec::new()),
+    ]);
+    let mut active_cata: Option<Item> = None;
+
+    /* Data search patterns. */
+    let cata_re = Regex::new(r"^([A-Za-z ]+):").unwrap();
+    let stats_re = Regex::new(r"^([A-Za-z0-9/+ ]+)\s+(\d+)\s+(\d+)\s+(\d+)").unwrap();
+
+    /* Open the file */
+    let file = File::open(file_path).unwrap();
+    let mut file_ptr = BufReader::new(file);
+
+    /* Process the file line by line. */
+    let mut buffer = String::new();
+    while file_ptr.read_line(&mut buffer).unwrap() > 0 {
+        /* Detect the start of a new shop catagory. */
+        if buffer.contains("Cost") && buffer.contains("Damage") && buffer.contains("Armor") {
+            let Some(captured_cata) = cata_re.captures(&buffer) else {
+                buffer.clear();
+                continue;
+            };
+
+            active_cata = match &captured_cata[1] {
+                "Weapons" => Some(Item::Weapon),
+                "Armor" => Some(Item::Armour),
+                "Rings" => Some(Item::Ring),
+                _ => panic!("Catagory could not be matched!"),
+            };
+        } else {
+            /* Extract the shop contents. */
+            let Some(item_stats) = stats_re.captures(&buffer) else {
+                buffer.clear();
+                continue;
+            };
+
+            shop.entry(active_cata.clone().unwrap())
+                .or_insert_with(Vec::new)
+                .push(ShopItem {
+                    name: String::from(item_stats[1].trim()),
+                    cost: item_stats[2].parse::<u32>().unwrap(),
+                    damage: item_stats[3].parse::<u32>().unwrap(),
+                    armour: item_stats[4].parse::<u32>().unwrap(),
+                    stype: active_cata.clone().unwrap(),
+                });
+        }
+        buffer.clear();
+    }
+
+    return shop;
 }
 
 /// Calculate the cost of a set of equipment
