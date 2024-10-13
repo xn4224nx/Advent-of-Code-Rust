6897ad8011f5e3e8e38aa2d28fd8c38c1e690c8e
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-10-13 22:44:04 +0100

    Setup for day 4.

---
+++ 2016/Day_04/src/main.rs
@@ -0,0 +1,45 @@
+/*
+ * --- Day 4: Security Through Obscurity ---
+ *
+ * Finally, you come across an information kiosk with a list of rooms. Of
+ * course, the list is encrypted and full of decoy data, but the instructions to
+ * decode the list are barely hidden nearby. Better remove the decoy data first.
+ *
+ * Each room consists of an encrypted name (lowercase letters separated by
+ * dashes) followed by a dash, a sector ID, and a checksum in square brackets.
+ *
+ * A room is real (not a decoy) if the checksum is the five most common letters
+ * in the encrypted name, in order, with ties broken by alphabetization.
+ *
+ * PART 1:  What is the sum of the sector IDs of the real rooms?
+ */
+
+pub struct Room {
+    pub encr_name: String,
+    pub sec_id: u32,
+    pub checksum: String,
+};
+
+impl Room {
+    pub fn new(encr_name: String, sec_id: u32, checksum: String) -> Self {
+        return Room {encr_name, sec_id, checksum}
+    }
+
+    pub fn verify(self) -> bool {
+        false
+    }
+}
+
+/// Read and parse room infomation from disk
+fn read_rooms(file_path: &str) -> Vec<Room> {
+    Vec::new()
+}
+
+/// Sum the sector ids of all real rooms
+fn sum_real_rooms(&Vec<Room>) -> u32 {
+    0
+}
+
+
+fn main() {
+}
---
+++ 2016/Day_04/tests/testing_main.rs
@@ -0,0 +1,45 @@
+#[allow(dead_code)]
+#[allow(unused_variables)]
+#[path = "../src/main.rs"]
+mod day_04;
+use day_04::{Room, sum_real_rooms, read_rooms};
+
+#[test]
+fn read_exp1_rooms() {
+    assert_eq!(read_rooms("./data/example_01.txt"), vec![
+        Room::new(String::from("aaaaa-bbb-z-y-x"), 123, String::from("abxyz")),
+        Room::new(String::from("a-b-c-d-e-f-g-h"), 987, String::from("abcde")),
+        Room::new(String::from("not-a-real-room"), 404, String::from("oarel")),
+        Room::new(String::from("totally-real-room"), 200, String::from("decoy")),
+    ])
+}
+
+#[test]
+fn verify_room_1() {
+    let test_rm = Room::new(String::from("aaaaa-bbb-z-y-x"), 123, String::from("abxyz"));
+    assert_eq!(test_rm.verify(), true);
+}
+
+#[test]
+fn verify_room_2() {
+    let test_rm = Room::new(String::from("a-b-c-d-e-f-g-h"), 987, String::from("abcde"));
+    assert_eq!(test_rm.verify(), true);
+}
+
+#[test]
+fn verify_room_3() {
+    let test_rm = Room::new(String::from("not-a-real-room"), 404, String::from("oarel"));
+    assert_eq!(test_rm.verify(), true);
+}
+
+#[test]
+fn verify_room_4() {
+    let test_rm = Room::new(String::from("totally-real-room"), 200, String::from("decoy"));
+    assert_eq!(test_rm.verify(), false);
+}
+
+#[test]
+fn sum_real__example_01_rooms() {
+    let exp1_rms = read_rooms("./data/example_01.txt");
+    assert_eq!(sum_real_rooms(&exp1_rms), 1514)
+}
