bae265617d20d2b36306c050a0a5166fd79b316c
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2024-11-10 01:10:09 +0000

    New function to extract markers.

--- 2016/Day_09/src/main.rs
+++ 2016/Day_09/src/main.rs
@@ -32,6 +32,71 @@ pub fn read_compressed_data(file_path: &str) -> Vec<u8> {
     return whole_file;
 }
 
+/// Determine the positions and values for the markers in a compressed data
+/// file. Each marker is represented by four integers, the marker starter index,
+/// the marker end index, the char range and the char repitition.
+pub fn find_markers(data: &Vec<u8>) -> Vec<(u32, u32, u32, u32)> {
+    let mut markers = Vec::new();
+    let mut in_marker = false;
+
+    /* Track the internal marker numbers. */
+    let mut part_num = Vec::new();
+    let mut range_num = 0;
+    let mut rep_num = 0;
+
+    /* Record the start and end index of the marker. */
+    let mut srt_idx = 0;
+
+    /* Iterate over every character. */
+    for (idx, d_char) in data.iter().enumerate() {
+        /* Detect the start of a marker, ie the '(' */
+        if *d_char == 40 {
+            if in_marker {
+                panic!("Nested markers are not supported!")
+            };
+
+            srt_idx = idx as u32;
+            in_marker = true;
+        }
+        /* Detect the end of the marker, ie the ')' */
+        else if *d_char == 41 {
+            if !in_marker {
+                panic!("Unmatched markers are not supported!")
+            };
+
+            /* Parse the second number of the marker. */
+            rep_num = String::from_utf8(part_num.clone())
+                .unwrap()
+                .parse::<u32>()
+                .unwrap();
+            part_num.clear();
+
+            /* Save the completed marker. */
+            markers.push((srt_idx, idx as u32, range_num, rep_num));
+
+            /* Reset the statistics ready for the next marker. */
+            range_num = 0;
+            rep_num = 0;
+            srt_idx = 0;
+
+            in_marker = false;
+        }
+        /* Detect the change in numbers of the marker, ie 'x' */
+        else if *d_char == 120 && in_marker {
+            range_num = String::from_utf8(part_num.clone())
+                .unwrap()
+                .parse::<u32>()
+                .unwrap();
+            part_num.clear();
+        }
+        /* Collect the numbers in the marker. */
+        else if in_marker && *d_char >= 48 && *d_char <= 57 {
+            part_num.push(*d_char)
+        }
+    }
+    return markers;
+}
+
 pub fn decompressed_len(data: &Vec<u8>) -> usize {
     0
 }
--- 2016/Day_09/tests/testing_main.rs
+++ 2016/Day_09/tests/testing_main.rs
@@ -2,7 +2,7 @@
 #[allow(unused_variables)]
 #[path = "../src/main.rs"]
 mod day_09;
-use day_09::{decompressed_len, read_compressed_data};
+use day_09::{decompressed_len, find_markers, read_compressed_data};
 
 #[test]
 fn read_exp_data_1() {
@@ -67,6 +67,85 @@ fn read_exp_data_8() {
         "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"
             .as_bytes()
             .to_vec()
+    );
+}
+
+#[test]
+fn find_markers_exp_data_1() {
+    assert_eq!(find_markers(&"ADVENT".as_bytes().to_vec()), Vec::new());
+}
+
+#[test]
+fn find_markers_exp_data_2() {
+    assert_eq!(
+        find_markers(&"A(1x5)BC".as_bytes().to_vec()),
+        vec![(1, 5, 1, 5)]
+    );
+}
+
+#[test]
+fn find_markers_exp_data_3() {
+    assert_eq!(
+        find_markers(&"(3x3)XYZ".as_bytes().to_vec()),
+        vec![(0, 4, 3, 3)]
+    );
+}
+
+#[test]
+fn find_markers_exp_data_4() {
+    assert_eq!(
+        find_markers(&"A(2x2)BCD(2x2)EFG".as_bytes().to_vec()),
+        vec![(1, 5, 2, 2), (9, 13, 2, 2)]
+    );
+}
+
+#[test]
+fn find_markers_exp_data_5() {
+    assert_eq!(
+        find_markers(&"(6x1)(1x3)A".as_bytes().to_vec()),
+        vec![(0, 4, 6, 1), (5, 9, 1, 3)]
+    );
+}
+
+#[test]
+fn find_markers_exp_data_6() {
+    assert_eq!(
+        find_markers(&"X(8x2)(3x3)ABCY".as_bytes().to_vec()),
+        vec![(1, 5, 8, 2), (6, 10, 3, 3)]
+    );
+}
+
+#[test]
+fn find_markers_exp_data_7() {
+    assert_eq!(
+        find_markers(&"(27x12)(20x12)(13x14)(7x10)(1x12)A".as_bytes().to_vec()),
+        vec![
+            (0, 6, 27, 12),
+            (7, 13, 20, 12),
+            (14, 20, 13, 14),
+            (21, 26, 7, 10),
+            (27, 32, 1, 12)
+        ]
+    );
+}
+
+#[test]
+fn find_markers_exp_data_8() {
+    assert_eq!(
+        find_markers(
+            &"(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"
+                .as_bytes()
+                .to_vec()
+        ),
+        vec![
+            (0, 5, 25, 3),
+            (6, 10, 3, 3),
+            (14, 18, 2, 3),
+            (21, 25, 5, 2),
+            (32, 37, 18, 9),
+            (38, 42, 3, 2),
+            (46, 50, 5, 7)
+        ]
     );
 }
 
