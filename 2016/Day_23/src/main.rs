a22a94f3bc079927651c731c00f62d8431956449
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2025-02-08 23:11:14 +0000

    Part 2 completed.

--- 2016/Day_23/src/main.rs
+++ 2016/Day_23/src/main.rs
@@ -51,6 +51,20 @@
  * register a to the safe.
  *
  * PART 1:  What value should be sent to the safe?
+ *
+ * The safe doesn't open, but it does make several angry noises to express its
+ * frustration.
+ *
+ * You're quite sure your logic is working correctly, so the only other thing
+ * is... you check the painting again. As it turns out, colored eggs are still
+ * eggs. Now you count 12.
+ *
+ * As you run the program with this new input, the prototype computer begins to
+ * overheat. You wonder what's taking so long, and whether the lack of any
+ * instruction more powerful than "add one" has anything to do with it. Don't
+ * bunnies usually multiply?
+ *
+ * PART 2:  Anyway, what value should actually be sent to the safe?
  */
 
 use regex::Regex;
@@ -255,5 +269,12 @@ impl Computer {
 }
 
 fn main() {
-    println!("Part 1 = {}", Computer::new("./data/input.txt").crack_safe(7));
+    println!(
+        "Part 1 = {}",
+        Computer::new("./data/input.txt").crack_safe(7)
+    );
+    println!(
+        "Part 2 = {}",
+        Computer::new("./data/input.txt").crack_safe(12)
+    );
 }
