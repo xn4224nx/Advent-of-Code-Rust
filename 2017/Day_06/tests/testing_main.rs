faf183dcb47cddd482acbdbae74404e6687c0928
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2025-03-16 17:59:32 +0000

    Setup for day 6.

---
+++ 2017/Day_06/tests/testing_main.rs
@@ -0,0 +1,69 @@
+#[allow(dead_code)]
+#[allow(unused_variables)]
+#[path = "../src/main.rs"]
+mod day_6;
+use day_6::MemBank;
+
+#[test]
+fn read_data_exp01() {
+    assert_eq!(
+        MemBank::new("./data/example_01.txt").blocks,
+        vec![0, 2, 7, 0]
+    );
+}
+
+#[test]
+fn read_data_exp02() {
+    assert_eq!(
+        MemBank::new("./data/input.txt").blocks,
+        vec![11, 11, 13, 7, 0, 15, 5, 5, 4, 4, 1, 1, 7, 1, 15, 11]
+    );
+}
+
+#[test]
+fn reallocate_exp01() {
+    let mut test_bank = MemBank::new("./data/example_01.txt");
+    test_bank.blocks = vec![0, 2, 7, 0];
+    test_bank.realocate();
+    assert_eq!(test_bank.blocks, vec![2, 4, 1, 2])
+}
+
+#[test]
+fn reallocate_exp02() {
+    let mut test_bank = MemBank::new("./data/example_01.txt");
+    test_bank.blocks = vec![2, 4, 1, 2];
+    test_bank.realocate();
+    assert_eq!(test_bank.blocks, vec![3, 1, 2, 3])
+}
+
+#[test]
+fn reallocate_exp03() {
+    let mut test_bank = MemBank::new("./data/example_01.txt");
+    test_bank.blocks = vec![3, 1, 2, 3];
+    test_bank.realocate();
+    assert_eq!(test_bank.blocks, vec![0, 2, 3, 4]);
+}
+
+#[test]
+fn reallocate_exp04() {
+    let mut test_bank = MemBank::new("./data/example_01.txt");
+    test_bank.blocks = vec![0, 2, 3, 4];
+    test_bank.realocate();
+    assert_eq!(test_bank.blocks, vec![1, 3, 4, 1]);
+}
+
+#[test]
+fn reallocate_exp05() {
+    let mut test_bank = MemBank::new("./data/example_01.txt");
+    test_bank.blocks = vec![1, 3, 4, 1];
+    test_bank.realocate();
+    assert_eq!(test_bank.blocks, vec![2, 4, 1, 2]);
+}
+
+#[test]
+fn cycles_to_duplicate_exp01() {
+    assert_eq!(
+        MemBank::new("./data/example_01.txt").cycles_to_duplicate(),
+        5
+    );
+}
