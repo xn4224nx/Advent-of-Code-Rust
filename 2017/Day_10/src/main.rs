0659388574cc7a4e95280d864ffe68843b23a70b
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2025-04-07 15:22:46 +0100

    Part 2 completed.

--- 2017/Day_10/src/main.rs
+++ 2017/Day_10/src/main.rs
@@ -82,9 +82,72 @@
  *          values 0 to 255) and the sequence of lengths in your puzzle input.
  *          Once this process is complete, what is the result of multiplying the
  *          first two numbers in the list?
+ *
+ * The logic you've constructed forms a single round of the Knot Hash algorithm;
+ * running the full thing requires many of these rounds. Some input and output
+ * processing is also required.
+ *
+ * First, from now on, your input should be taken not as a list of numbers, but
+ * as a string of bytes instead. Unless otherwise specified, convert characters
+ * to bytes using their ASCII codes. This will allow you to handle arbitrary
+ * ASCII strings, and it also ensures that your input lengths are never larger
+ * than 255. For example, if you are given 1,2,3, you should convert it to the
+ * ASCII codes for each character: 49,44,50,44,51.
+ *
+ * Once you have determined the sequence of lengths to use, add the following
+ * lengths to the end of the sequence: 17, 31, 73, 47, 23. For example, if you
+ * are given 1,2,3, your final sequence of lengths should be
+ * 49,44,50,44,51,17,31,73,47,23 (the ASCII codes from the input string combined
+ * with the standard length suffix values).
+ *
+ * Second, instead of merely running one round like you did above, run a total
+ * of 64 rounds, using the same length sequence in each round. The current
+ * position and skip size should be preserved between rounds. For example, if
+ * the previous example was your first round, you would start your second round
+ * with the same length sequence (3, 4, 1, 5, 17, 31, 73, 47, 23, now assuming
+ * they came from ASCII codes and include the suffix), but start with the
+ * previous round's current position (4) and skip size (4).
+ *
+ * Once the rounds are complete, you will be left with the numbers from 0 to 255
+ * in some order, called the sparse hash. Your next task is to reduce these to a
+ * list of only 16 numbers called the dense hash. To do this, use numeric
+ * bitwise XOR to combine each consecutive block of 16 numbers in the sparse
+ * hash (there are 16 such blocks in a list of 256 numbers). So, the first
+ * element in the dense hash is the first sixteen elements of the sparse hash
+ * XOR'd together, the second element in the dense hash is the second sixteen
+ * elements of the sparse hash XOR'd together, etc.
+ *
+ * For example, if the first sixteen elements of your sparse hash are as shown
+ * below, and the XOR operator is ^, you would calculate the first output number
+ * like this:
+ *
+ * 65 ^ 27 ^ 9 ^ 1 ^ 4 ^ 3 ^ 40 ^ 50 ^ 91 ^ 7 ^ 6 ^ 0 ^ 2 ^ 5 ^ 68 ^ 22 = 64
+ *
+ * Perform this operation on each of the sixteen blocks of sixteen numbers in
+ * your sparse hash to determine the sixteen numbers in your dense hash.
+ *
+ * Finally, the standard way to represent a Knot Hash is as a single hexadecimal
+ * string; the final output is the dense hash in hexadecimal notation. Because
+ * each number in your dense hash will be between 0 and 255 (inclusive), always
+ * represent each number as two hexadecimal digits (including a leading zero as
+ * necessary). So, if your first three numbers are 64, 7, 255, they correspond
+ * to the hexadecimal numbers 40, 07, ff, and so the first six characters of the
+ * hash would be 4007ff. Because every Knot Hash is sixteen such numbers, the
+ * hexadecimal representation is always 32 hexadecimal digits (0-f) long.
+ *
+ * Here are some example hashes:
+ *
+ *      -   The empty string becomes a2582a3a0e66e6e86e3812dcb672a272.
+ *      -   AoC 2017 becomes 33efeb34ea91902bb2f59c9920caa6cd.
+ *      -   1,2,3 becomes 3efbe78a8d82f29979031a4aa0b16a9d.
+ *      -   1,2,4 becomes 63960835bcdc130f0b66d7ff4f6a5a8e.
+ *
+ * PART 1:  Treating your puzzle input as a string of ASCII characters, what is
+ *          the Knot Hash of your puzzle input? Ignore any leading or trailing
+ *          whitespace you might encounter.
  */
 
-use std::fs::read_to_string;
+use std::fs::{read, read_to_string};
 
 pub struct KnotHash {
     pub nums: Vec<u32>,
@@ -94,17 +157,35 @@ pub struct KnotHash {
 }
 
 impl KnotHash {
-    pub fn new(twist_file: &str, max_num: u32) -> Self {
-        KnotHash {
-            nums: (0..=max_num).collect(),
-            twists: read_to_string(twist_file)
+    pub fn new(twist_file: &str, max_num: u32, ascii: bool) -> Self {
+        let twists = if ascii {
+            let mut tmp: Vec<usize> = read(twist_file)
+                .unwrap()
+                .into_iter()
+                .map(|x| x as usize)
+                .collect();
+
+            /* If the file is not empty remove the final char. */
+            if tmp.len() > 0 {
+                tmp.remove(tmp.len() - 1);
+            }
+
+            tmp.extend(vec![17, 31, 73, 47, 23]);
+            tmp
+        } else {
+            read_to_string(twist_file)
                 .unwrap()
                 .split(",")
                 .map(|x| x.trim().parse::<usize>().unwrap())
-                .collect(),
+                .collect()
+        };
+
+        return KnotHash {
+            nums: (0..=max_num).collect(),
+            twists,
             skip_size: 0,
             curr_pos: 0,
-        }
+        };
     }
 
     /// Reverse a section of the numbers
@@ -129,9 +210,6 @@ impl KnotHash {
 
     /// Perform a single round of the hash
     pub fn run_round(&mut self) {
-        self.skip_size = 0;
-        self.curr_pos = 0;
-
         for idx in 0..self.twists.len() {
             self.reverse(self.twists[idx]);
         }
@@ -142,11 +220,34 @@ impl KnotHash {
         self.run_round();
         return self.nums[0] * self.nums[1];
     }
+
+    /// Determine the final digest of the message
+    pub fn digest(&mut self) -> String {
+        let num_rounds = 64;
+        let block_size = 16;
+        let mut sparse_digest: Vec<String> = Vec::new();
+
+        /* Run rounds and presereve the skip and position. */
+        for _ in 0..num_rounds {
+            self.run_round();
+        }
+
+        /* XOR each block of numbers into one number. */
+        for blk_idx in 0..block_size {
+            let mut tmp_blck = 0;
+            for num_idx in 0..block_size {
+                tmp_blck ^= self.nums[blk_idx * block_size + num_idx];
+            }
+            sparse_digest.push(format!("{:02x}", tmp_blck)[..2].to_string())
+        }
+        return sparse_digest.join("");
+    }
 }
 
 fn main() {
     println!(
-        "Part 1 = {}",
-        KnotHash::new("./data/input.txt", 255).verify_round_hash()
+        "Part 1 = {}\nPart 2 = {}",
+        KnotHash::new("./data/input.txt", 255, false).verify_round_hash(),
+        KnotHash::new("./data/input.txt", 255, true).digest()
     );
 }
--- 2017/Day_10/tests/testing_main.rs
+++ 2017/Day_10/tests/testing_main.rs
@@ -6,7 +6,7 @@ use day_10::KnotHash;
 
 #[test]
 fn new_class_exp01() {
-    let test = KnotHash::new("./data/example_01.txt", 4);
+    let test = KnotHash::new("./data/example_01.txt", 4, false);
     assert_eq!(test.skip_size, 0);
     assert_eq!(test.curr_pos, 0);
     assert_eq!(test.nums, vec![0, 1, 2, 3, 4]);
@@ -15,7 +15,7 @@ fn new_class_exp01() {
 
 #[test]
 fn new_class_exp02() {
-    let test = KnotHash::new("./data/example_02.txt", 4);
+    let test = KnotHash::new("./data/example_02.txt", 4, false);
     assert_eq!(test.skip_size, 0);
     assert_eq!(test.curr_pos, 0);
     assert_eq!(test.nums, vec![0, 1, 2, 3, 4]);
@@ -29,7 +29,7 @@ fn new_class_exp02() {
 
 #[test]
 fn reverse_exp01() {
-    let mut test = KnotHash::new("./data/example_01.txt", 4);
+    let mut test = KnotHash::new("./data/example_01.txt", 4, false);
     test.curr_pos = 0;
     test.skip_size = 0;
     test.nums = vec![0, 1, 2, 3, 4];
@@ -41,7 +41,7 @@ fn reverse_exp01() {
 
 #[test]
 fn reverse_exp02() {
-    let mut test = KnotHash::new("./data/example_01.txt", 4);
+    let mut test = KnotHash::new("./data/example_01.txt", 4, false);
     test.curr_pos = 3;
     test.skip_size = 1;
     test.nums = vec![2, 1, 0, 3, 4];
@@ -53,7 +53,7 @@ fn reverse_exp02() {
 
 #[test]
 fn reverse_exp03() {
-    let mut test = KnotHash::new("./data/example_01.txt", 4);
+    let mut test = KnotHash::new("./data/example_01.txt", 4, false);
     test.curr_pos = 3;
     test.skip_size = 2;
     test.nums = vec![4, 3, 0, 1, 2];
@@ -65,7 +65,7 @@ fn reverse_exp03() {
 
 #[test]
 fn reverse_exp04() {
-    let mut test = KnotHash::new("./data/example_01.txt", 4);
+    let mut test = KnotHash::new("./data/example_01.txt", 4, false);
     test.curr_pos = 1;
     test.skip_size = 3;
     test.nums = vec![4, 3, 0, 1, 2];
@@ -77,9 +77,57 @@ fn reverse_exp04() {
 
 #[test]
 fn run_a_round_exp01() {
-    let mut test = KnotHash::new("./data/example_01.txt", 4);
+    let mut test = KnotHash::new("./data/example_01.txt", 4, false);
     test.run_round();
     assert_eq!(test.skip_size, 4);
     assert_eq!(test.curr_pos, 4);
     assert_eq!(test.nums, vec![3, 4, 2, 1, 0]);
+}
+
+#[test]
+fn read_twist_as_ascii_exp01() {
+    assert_eq!(
+        KnotHash::new("./data/example_05.txt", 4, true).twists,
+        vec![49, 44, 50, 44, 51, 17, 31, 73, 47, 23]
+    );
+}
+
+#[test]
+fn read_twist_as_ascii_exp02() {
+    assert_eq!(
+        KnotHash::new("./data/example_03.txt", 4, true).twists,
+        vec![17, 31, 73, 47, 23]
+    );
+}
+
+#[test]
+fn digest_exp03() {
+    assert_eq!(
+        KnotHash::new("./data/example_03.txt", 255, true).digest(),
+        String::from("a2582a3a0e66e6e86e3812dcb672a272")
+    );
+}
+
+#[test]
+fn digest_exp04() {
+    assert_eq!(
+        KnotHash::new("./data/example_04.txt", 255, true).digest(),
+        String::from("33efeb34ea91902bb2f59c9920caa6cd")
+    );
+}
+
+#[test]
+fn digest_exp05() {
+    assert_eq!(
+        KnotHash::new("./data/example_05.txt", 255, true).digest(),
+        String::from("3efbe78a8d82f29979031a4aa0b16a9d")
+    );
+}
+
+#[test]
+fn digest_exp06() {
+    assert_eq!(
+        KnotHash::new("./data/example_06.txt", 255, true).digest(),
+        String::from("63960835bcdc130f0b66d7ff4f6a5a8e")
+    );
 }
