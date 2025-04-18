b84d7ee03c0377c3656c5726ef694e84be6db338
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2025-04-18 15:46:27 +0100

    Setup for day 12.

---
+++ 2017/Day_12/tests/testing_main.rs
@@ -0,0 +1,78 @@
+#[allow(dead_code)]
+#[allow(unused_variables)]
+#[path = "../src/main.rs"]
+mod day_11;
+use day_11::ProgramVillage;
+use std::collections::{HashMap, HashSet};
+
+#[test]
+fn parse_data_exp01() {
+    assert_eq!(
+        ProgramVillage::new("./data/example_01.txt").links,
+        HashMap::from([
+            (0, HashSet::from([2])),
+            (1, HashSet::new()),
+            (2, HashSet::from([0, 3, 4])),
+            (3, HashSet::from([2, 4])),
+            (4, HashSet::from([2, 3, 6])),
+            (5, HashSet::from([6])),
+            (6, HashSet::from([4, 5])),
+        ])
+    );
+}
+
+#[test]
+fn group_census_exp01() {
+    assert_eq!(
+        ProgramVillage::new("./data/example_01.txt").full_prog_group(0),
+        HashSet::from([0, 2, 3, 4, 5, 6])
+    );
+}
+
+#[test]
+fn group_census_exp02() {
+    assert_eq!(
+        ProgramVillage::new("./data/example_01.txt").full_prog_group(1),
+        HashSet::from([1])
+    );
+}
+
+#[test]
+fn group_census_exp03() {
+    assert_eq!(
+        ProgramVillage::new("./data/example_01.txt").full_prog_group(2),
+        HashSet::from([0, 2, 3, 4, 5, 6])
+    );
+}
+
+#[test]
+fn group_census_exp04() {
+    assert_eq!(
+        ProgramVillage::new("./data/example_01.txt").full_prog_group(3),
+        HashSet::from([0, 2, 3, 4, 5, 6])
+    );
+}
+
+#[test]
+fn group_census_exp05() {
+    assert_eq!(
+        ProgramVillage::new("./data/example_01.txt").full_prog_group(4),
+        HashSet::from([0, 2, 3, 4, 5, 6])
+    );
+}
+
+#[test]
+fn group_census_exp06() {
+    assert_eq!(
+        ProgramVillage::new("./data/example_01.txt").full_prog_group(5),
+        HashSet::from([0, 2, 3, 4, 5, 6])
+    );
+}
+
+#[test]
+fn group_census_exp07() {
+    assert_eq!(
+        ProgramVillage::new("./data/example_01.txt").full_prog_group(6),
+        HashSet::from([0, 2, 3, 4, 5, 6])
+    );
+}
