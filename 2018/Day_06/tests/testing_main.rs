7b7381b6cffacf5c9770fe18a218cee5026065e4
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2025-08-10 19:39:25 +0100

    Setup for day 6.

---
+++ 2018/Day_06/tests/testing_main.rs
@@ -0,0 +1,24 @@
+#[allow(dead_code)]
+#[allow(unused_variables)]
+#[path = "../src/main.rs"]
+mod main;
+use crate::main::MineField;
+
+#[test]
+fn read_minefield_exp0() {
+    let test = MineField::new("./data/example_0.txt");
+    assert_eq!(
+        test.mine_coords,
+        vec![(1, 1), (1, 6), (8, 3), (3, 4), (5, 5), (8, 9),]
+    );
+    assert_eq!(test.top_left_corner, (1, 1));
+    assert_eq!(test.bottom_right_corner, (8, 9));
+}
+
+#[test]
+fn largest_connected_area_exp0() {
+    assert_eq!(
+        MineField::new("./data/example_0.txt").largest_connected_area(),
+        17
+    )
+}
