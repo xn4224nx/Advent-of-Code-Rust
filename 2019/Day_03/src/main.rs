cf4ab9342f7f86b704329b6e2969c06266898408
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2025-10-12 15:48:12 +0100

    All tests passed.

--- 2019/Day_03/src/main.rs
+++ 2019/Day_03/src/main.rs
@@ -60,7 +60,9 @@
  *          intersection?
  */
 
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 pub struct WireInteraction {
     pub wire_loc_dists: Vec<HashMap<(i32, i32), usize>>,
@@ -68,13 +70,78 @@ pub struct WireInteraction {
 
 impl WireInteraction {
     pub fn from_file(directs_file: &str) -> Self {
-        return WireInteraction {
-            wire_loc_dists: Vec::new(),
-        };
+        let mut buffer = String::new();
+        let mut wire_loc_dists = Vec::new();
+
+        /* Open the file. */
+        let file = File::open(directs_file).unwrap();
+        let mut fp = BufReader::new(file);
+
+        /* Each line of the file will be the complete path of a single wire. */
+        while fp.read_line(&mut buffer).unwrap() > 0 {
+            let mut all_dists = HashMap::new();
+
+            /* Start the wire at an origin. */
+            let mut curr_pnt = (0, 0);
+
+            /* Keep track of the length of the wire. */
+            let mut curr_wire_len = 0;
+
+            /* Move the point based on the direction and distance given. */
+            for raw_move in buffer.trim_end().split(",") {
+                let raw = raw_move.chars().collect::<Vec<char>>();
+                let dist = raw[1..]
+                    .iter()
+                    .collect::<String>()
+                    .parse::<usize>()
+                    .unwrap();
+
+                /* Follow the wire along its directed path */
+                for _ in 0..dist {
+                    curr_wire_len += 1;
+
+                    if raw[0] == 'U' {
+                        curr_pnt = (curr_pnt.0, curr_pnt.1 + 1)
+                    } else if raw[0] == 'D' {
+                        curr_pnt = (curr_pnt.0, curr_pnt.1 - 1)
+                    } else if raw[0] == 'L' {
+                        curr_pnt = (curr_pnt.0 - 1, curr_pnt.1)
+                    } else if raw[0] == 'R' {
+                        curr_pnt = (curr_pnt.0 + 1, curr_pnt.1)
+                    } else {
+                        panic!("Unknown direction: '{}'", raw[0]);
+                    }
+
+                    /* Save the new position the wire has reached. */
+                    all_dists.insert(curr_pnt, curr_wire_len);
+                }
+            }
+            wire_loc_dists.push(all_dists);
+            buffer.clear();
+        }
+        return WireInteraction { wire_loc_dists };
     }
 
     pub fn short_circuit_dist(&self) -> usize {
-        0
+        let mut crossed_pnts = HashSet::new();
+
+        /* Find all the points where the wires cross. */
+        for pnt_0 in self.wire_loc_dists[0].keys() {
+            if crossed_pnts.contains(pnt_0) {
+                continue;
+            }
+
+            if self.wire_loc_dists[1].contains_key(pnt_0) {
+                crossed_pnts.insert(pnt_0);
+            }
+        }
+
+        /* Find the point of crossing closest to the origin. */
+        return crossed_pnts
+            .iter()
+            .map(|(x, y)| x.abs() + y.abs())
+            .min()
+            .unwrap() as usize;
     }
 }
 
