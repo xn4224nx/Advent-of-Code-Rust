5e961d162c43f9dd6135c93f8f3db1235c6d979b
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2025-10-13 02:27:04 +0100

    Part 2 completed.

--- 2019/Day_04/src/main.rs
+++ 2019/Day_04/src/main.rs
@@ -26,10 +26,28 @@
  *
  * PART 1:  How many different passwords within the range given in your puzzle
  *          input meet these criteria?
+ *
+ * An Elf just remembered one more important detail: the two adjacent matching
+ * digits are not part of a larger group of matching digits.
+ *
+ * Given this additional criterion, but still ignoring the range rule, the
+ * following are now true:
+ *
+ *      -   112233 meets these criteria because the digits never decrease and
+ *          all repeated digits are exactly two digits long.
+ *
+ *      -   123444 no longer meets the criteria (the repeated 44 is part of a
+ *          larger group of 444).
+ *
+ *      -   111122 meets the criteria (even though 1 is repeated more than
+ *          twice, it still contains a double 22).
+ *
+ * PART 2:  How many different passwords within the range given in your puzzle
+ *          input meet all of the criteria?
  */
 
 /// Does the provided number meet the password rules
-pub fn is_valid_num(number: &Vec<usize>) -> bool {
+pub fn is_valid_num(number: &Vec<usize>, triples_ok: bool) -> bool {
     let mut double_seen: bool = false;
 
     /* It is a six-digit number. */
@@ -40,6 +58,15 @@ pub fn is_valid_num(number: &Vec<usize>) -> bool {
     for dig_idx in 1..number.len() {
         /* Look for any doubles. */
         if !double_seen && number[dig_idx] == number[dig_idx - 1] {
+            /* Check the number before this pair */
+            if !triples_ok && dig_idx > 1 && number[dig_idx - 2] == number[dig_idx - 1] {
+                continue;
+            }
+
+            /* Check the number after this pair */
+            if !triples_ok && dig_idx < number.len() - 1 && number[dig_idx + 1] == number[dig_idx] {
+                continue;
+            }
             double_seen = true;
         }
 
@@ -52,7 +79,7 @@ pub fn is_valid_num(number: &Vec<usize>) -> bool {
 }
 
 /// How many valid passwords are in the supplied range (inclusive)?
-pub fn count_valid_nums(lower_lim: usize, upper_lim: usize) -> usize {
+pub fn count_valid_nums(lower_lim: usize, upper_lim: usize, triples_ok: bool) -> usize {
     return (lower_lim..=upper_lim)
         .filter(|x| {
             is_valid_num(
@@ -60,11 +87,16 @@ pub fn count_valid_nums(lower_lim: usize, upper_lim: usize) -> usize {
                     .chars()
                     .map(|y| y.to_digit(10).unwrap() as usize)
                     .collect::<Vec<usize>>(),
+                triples_ok,
             )
         })
         .count();
 }
 
 fn main() {
-    println!("Part 1 = {}", count_valid_nums(254032, 789860));
+    println!(
+        "Part 1 = {}\nPart 2 = {}\n",
+        count_valid_nums(254032, 789860, true),
+        count_valid_nums(254032, 789860, false)
+    );
 }
--- 2019/Day_04/tests/testing_main.rs
+++ 2019/Day_04/tests/testing_main.rs
@@ -6,30 +6,45 @@ use main::is_valid_num;
 
 #[test]
 fn test_is_valid_num_exp0() {
-    assert_eq!(is_valid_num(&vec![1, 2, 2, 3, 4, 5]), true);
+    assert_eq!(is_valid_num(&vec![1, 2, 2, 3, 4, 5], true), true);
 }
 
 #[test]
 fn test_is_valid_num_exp1() {
-    assert_eq!(is_valid_num(&vec![1, 1, 1, 1, 2, 3]), true);
+    assert_eq!(is_valid_num(&vec![1, 1, 1, 1, 2, 3], true), true);
 }
 
 #[test]
 fn test_is_valid_num_exp2() {
-    assert_eq!(is_valid_num(&vec![1, 3, 5, 6, 7, 9]), false);
+    assert_eq!(is_valid_num(&vec![1, 3, 5, 6, 7, 9], true), false);
 }
 
 #[test]
 fn test_is_valid_num_exp3() {
-    assert_eq!(is_valid_num(&vec![1, 1, 1, 1, 1, 1]), true);
+    assert_eq!(is_valid_num(&vec![1, 1, 1, 1, 1, 1], true), true);
 }
 
 #[test]
 fn test_is_valid_num_exp4() {
-    assert_eq!(is_valid_num(&vec![2, 2, 3, 4, 5, 0]), false);
+    assert_eq!(is_valid_num(&vec![2, 2, 3, 4, 5, 0], true), false);
 }
 
 #[test]
 fn test_is_valid_num_exp5() {
-    assert_eq!(is_valid_num(&vec![1, 2, 3, 7, 8, 9]), false);
+    assert_eq!(is_valid_num(&vec![1, 2, 3, 7, 8, 9], true), false);
+}
+
+#[test]
+fn test_is_valid_num_exp6() {
+    assert_eq!(is_valid_num(&vec![1, 1, 2, 2, 3, 3], false), true);
+}
+
+#[test]
+fn test_is_valid_num_exp7() {
+    assert_eq!(is_valid_num(&vec![1, 2, 3, 4, 4, 4], false), false);
+}
+
+#[test]
+fn test_is_valid_num_exp8() {
+    assert_eq!(is_valid_num(&vec![1, 1, 1, 1, 2, 2], false), true);
 }
